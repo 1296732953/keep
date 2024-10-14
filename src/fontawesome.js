8a4058c45c616e2970b8f0e2618f8659e6517efe
Evan <144076043+1296732953@users.noreply.github.com>
2024-10-14 18:37:30 +0800

    Delete src directory

--- src/fontawesome.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronLeft, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faChevronLeft, faEllipsisV);
